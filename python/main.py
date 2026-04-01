2aa0cf5e79bd86771d75232933f6f0bc5255478a
ali-rasteh <ali.rasteh1@gmail.com>
2026-04-01 15:34:12 -0400

    debug

--- python/main.py
+++ python/main.py
@@ -1,19 +1,13 @@
-# import matplotlib
-# Select backend before importing modules that may import matplotlib.pyplot.
-# matplotlib.use("WebAgg")
-# matplotlib.rcParams["webagg.open_in_browser"] = True
-# matplotlib.use('TkAgg')
-# matplotlib.use('Agg')
+from parser_utils import apply_cli_overrides
+from sounder import Sounder, SounderConfig
+from sounder_configs import FR3RoboticLocalizationConfig, FR3SpectrumSweepConfig, TestConfig
 
 
 def main():
-    from parser_utils import apply_cli_overrides
-    from sigcom_toolkit.base import DebugMode
-    from sounder import Sounder, SounderConfig
-    from sounder_configs import FR3RoboticLocalizationConfig, FR3SpectrumSweepConfig, TestConfig
-
     config = TestConfig()
     apply_cli_overrides(config, SounderConfig)
+
+    # from sigcom_toolkit.base import DebugMode
     # config.debug_mode = DebugMode.HIGH
 
     sounder = Sounder(config)
